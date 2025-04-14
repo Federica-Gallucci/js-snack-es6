52d1d2bf24de2e61a2aeda6485cd240e01a3d917
Federica-Gallucci <gallucci.federica.c@gmail.com>
2025-04-14 16:05:03 +0200

    add snack 1

--- js/script.js
+++ js/script.js
@@ -3,6 +3,47 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo la bici con peso minore.
 
+const bike = [
+  {
+    name: "Bianchi",
+    weight: 7.4,
+  },
+  {
+    name: "Look",
+    weight: 6.81,
+  },
+  {
+    name: "Willier",
+    weight: 6.5,
+  },
+  {
+    name: "Pinarello",
+    weight: 8.1,
+  },
+  {
+    name: "Colnago",
+    weight: 5.5,
+  },
+  {
+    name: "Canyon",
+    weight: 8,
+  },
+  {
+    name: "Trek",
+    weight: 4.65,
+  },
+];
+
+let lessWeightBike = bike[0];
+
+for (let i = 0; i < bike.length; i++) {
+  let currentBike = bike[i];
+  if (currentBike.weight < lessWeightBike.weight) {
+    lessWeightBike = currentBike;
+  }
+}
+console.log(lessWeightBike);
+
 // ? Snack2
 // Creare un array di oggetti di squadre di calcio.
 // Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
